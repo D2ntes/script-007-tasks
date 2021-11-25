2a9174ca3ecfc7ae0992093359e438faf647c754
Nikita Gruzdev <46005116+D2ntes@users.noreply.github.com>
2021-11-25 12:56:45 +0300

    add function in FileServer

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
+import argparse
+import os
+
+import server.FileService as fs
+
+
 def main():
-    pass
+    """Entry point of app.
+    Get and parse command line parameters and configure web app.
+    Command line options:
+    -d --dir  - working directory (absolute or relative path, default: current_app_folder/data).
+    -h --help - help.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dir', default=os.path.join(os.getcwd(), 'data'), type=str,
+                        help="working directory (default: 'data')")
+
+    params = parser.parse_args()
+
+    fs.change_dir(params.dir)
 
 
 if __name__ == "__main__":
--- server/FileService.py
+++ server/FileService.py
@@ -1,5 +1,13 @@
+import os
+from datetime import datetime
+import errno
+import sys
+import shutil
 
-def change_dir(path: str, autocreate: bool = True) -> None:
+ERROR_INVALID_NAME = 123
+
+
+def change_dir(path: str = None, autocreate: bool = True) -> None:
     """Change current directory of app.
 
     Args:
@@ -9,9 +17,20 @@ def change_dir(path: str, autocreate: bool = True) -> None:
     Raises:
         RuntimeError: if directory does not exist and autocreate is False.
         ValueError: if path is invalid.
+    Returns:
+        Path of current directory
     """
-
-    pass
+    if not path:
+        return os.getcwd()
+    if not _check_path(path):
+        raise ValueError(f'Path "{path}" is invalid')
+    if not os.path.exists(path):
+        if autocreate:
+            os.mkdir(path)
+        else:
+            raise RuntimeError(f'Directory "{path}" does not exist and autocreate is False')
+    os.chdir(path)
+    return os.getcwd()
 
 
 def get_files() -> list:
@@ -24,8 +43,19 @@ def get_files() -> list:
         - edit_date (datetime): date of last file modification.
         - size (int): size of file in bytes.
     """
-
-    pass
+    file_list = []
+    filepath_list = os.listdir(os.getcwd())
+    for file in filepath_list:
+        try:
+            file_list.append([
+                              file,
+                              datetime.fromtimestamp(os.stat(file).st_ctime),
+                              datetime.fromtimestamp(os.stat(file).st_mtime),
+                              os.stat(file).st_size
+                             ])
+        except FileNotFoundError as err:
+            file_list.append([file, None, None, None])
+    return file_list
 
 
 def get_file_data(filename: str) -> dict:
@@ -46,16 +76,28 @@ def get_file_data(filename: str) -> dict:
         RuntimeError: if file does not exist.
         ValueError: if filename is invalid.
     """
+    path = os.path.join(os.getcwd(), filename)
+    if not _check_path(path):
+        raise ValueError(f'Filename "{filename}" is invalid')
+    if not os.path.exists(path):
+        raise ValueError(f'Filename "{filename}" does not exist')
+    with open(path, 'rb') as file:
+        return {
+                'name': filename,
+                'content': file.read(),
+                'create_date': datetime.fromtimestamp(os.stat(path).st_ctime),
+                'edit_date': datetime.fromtimestamp(os.stat(path).st_mtime),
+                'size': os.stat(path).st_size
+                }
 
-    pass
 
-
-def create_file(filename: str, content: str = None) -> dict:
+def create_file(filename: str, content: str = '', overwrite: bool = False) -> dict:
     """Create a new file.
 
     Args:
         filename (str): Filename.
         content (str): String with file content.
+        overwrite (bool): Create file if it already exist
 
     Returns:
         Dict, which contains name of created file. Keys:
@@ -66,9 +108,34 @@ def create_file(filename: str, content: str = None) -> dict:
 
     Raises:
         ValueError: if filename is invalid.
+        RuntimeError: if file already exist and overwrite is False.
+        OSError: if incorrect file creation
     """
 
-    pass
+    path = os.path.join(os.getcwd(), filename)
+
+    if not _check_path(path):
+        raise ValueError(f'Filename "{filename}" is invalid')
+
+    if os.path.exists(path) and not overwrite:
+        raise RuntimeError(f'File "{path}" already exist and overwrite is False')
+
+    with open(path, 'wb') as file:
+        if content:
+            data = bytes(content, encoding='utf-8')
+            file.write(data)
+
+    with open(path, 'r') as file:
+        if file.read() != content:
+            raise OSError(f'Incorrect creation of file "{path}"')
+
+        return {
+                'name': filename,
+                'content': bytes(content, encoding='utf-8'),
+                'create_date': datetime.fromtimestamp(os.stat(path).st_ctime),
+                'edit_date': datetime.fromtimestamp(os.stat(path).st_mtime),
+                'size': os.stat(path).st_size
+                }
 
 
 def delete_file(filename: str) -> None:
@@ -82,4 +149,51 @@ def delete_file(filename: str) -> None:
         ValueError: if filename is invalid.
     """
 
-    pass
+    path = os.path.join(os.getcwd(), filename)
+    if not os.path.exists(path):
+        raise RuntimeError('File {} does not exist'.format(filename))
+
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+    else:
+        os.remove(path)
+
+
+def _check_path(path: str) -> bool:
+    """
+    `True` if the passed pathname is a valid pathname for the current OS;
+    `False` otherwise.
+    """
+    try:
+        if not isinstance(path, str) or not path:
+            return False
+
+        _, pathname = os.path.splitdrive(path)
+
+        root_dirname = os.environ.get('HOMEDRIVE', 'C:') if sys.platform == 'win32' else os.path.sep
+        assert os.path.isdir(root_dirname)
+
+        root_dirname = root_dirname.rstrip(os.path.sep) + os.path.sep
+        for pathname_part in pathname.split(os.path.sep):
+            try:
+                os.lstat(root_dirname + pathname_part)
+            except OSError as err:
+                if hasattr(err, 'winerror'):
+                    if err.winerror == ERROR_INVALID_NAME:
+                        return False
+                elif err.errno in {errno.ENAMETOOLONG}:
+                    return False
+    except TypeError:
+        return False
+    else:
+        return True
+
+
+if __name__ == '__main__':
+    print(os.getcwd())
+    print(get_files())
+    print(os.getcwd())
+    print(create_file('sdfs1.txt', 'sed9869fs', overwrite=True))
+    delete_file('1.txt')
+
+
